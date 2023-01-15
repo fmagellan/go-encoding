3a76105b2a38b803c6043fae7cf5846390522844
fmagellan <>
2023-01-15 20:19:05 +0530

    Usage of varint encoding

--- client1.go
+++ client1.go
@@ -14,11 +14,40 @@ func main() {
 	}
 	defer c.Close()
 
+	cFixedStructWithReflection(c)
+	cStructWithVarInt(c)
+}
+
+func cFixedStructWithReflection(c net.Conn) {
+	_, _ = c.Write([]byte("FixedStructWithReflection"))
+
 	s := new(utils.Numbers)
-	err = binary.Read(c, binary.BigEndian, s)
+	err := binary.Read(c, binary.BigEndian, s)
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Printf("data: %v\n", s)
+}
+
+func cStructWithVarInt(c net.Conn) {
+	_, _ = c.Write([]byte("StructWithVarInt"))
+
+	b := make([]byte, 1024)
+	n, err := c.Read(b)
+	if err != nil {
+		panic(err)
+	}
+
+	n1, n1Bytes := binary.Varint(b)
+	n2, n2Bytes := binary.Varint(b[n1Bytes:])
+	n3, n3Bytes := binary.Varint(b[n1Bytes+n2Bytes:])
+
+	s := utils.Numbers{
+		N1: int32(n1),
+		N2: int8(n2),
+		N3: uint16(n3),
+	}
+	fmt.Printf("number of bytes read: %v, var int bytes decoded: %v\n", n, n1Bytes+n2Bytes+n3Bytes)
 	fmt.Printf("data: %v\n", s)
 }
--- server1.go
+++ server1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/binary"
+	"fmt"
 	"github.com/fmagellan/go-encoding/utils"
 	"net"
 )
@@ -19,13 +20,53 @@ func main() {
 	}
 	defer conn.Close()
 
+	b := make([]byte, 1024)
+mainloop:
+	for i := 0; i < 2; i++ {
+		n, err := conn.Read(b)
+		if err != nil {
+			panic(err)
+		}
+
+		op := string(b[:n])
+		fmt.Printf("op: %v\n", op)
+		switch op {
+		case "FixedStructWithReflection":
+			sFixedStructWithReflection(conn)
+		case "StructWithVarInt":
+			sStructWithVarInt(conn)
+		default:
+			fmt.Printf("invalid op: %v\n", op)
+			break mainloop
+		}
+	}
+}
+
+func sFixedStructWithReflection(conn net.Conn) {
 	s := utils.Numbers{
 		N1: 10,
 		N2: 11,
 		N3: 12,
 	}
 
-	err = binary.Write(conn, binary.BigEndian, s)
+	err := binary.Write(conn, binary.BigEndian, s)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func sStructWithVarInt(conn net.Conn) {
+	s := utils.Numbers{
+		N1: 10,
+		N2: 11,
+		N3: 12,
+	}
+
+	b := make([]byte, 1024)
+	n := binary.PutVarint(b, int64(s.N1))
+	n += binary.PutVarint(b[n:], int64(s.N2))
+	n += binary.PutVarint(b[n:], int64(s.N3))
+	_, err := conn.Write(b[:n])
 	if err != nil {
 		panic(err)
 	}
